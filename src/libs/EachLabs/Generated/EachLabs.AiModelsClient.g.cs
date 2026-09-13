5fadb078bb22b93589cb537f8325100966f84c21
Konstantin S. <havendv@gmail.com>
2026-09-13 21:55:05 +0400

    feat: Updated OpenAPI spec (#48)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/EachLabs/Generated/EachLabs.AiModelsClient.g.cs
+++ src/libs/EachLabs/Generated/EachLabs.AiModelsClient.g.cs
@@ -35,7 +35,7 @@ namespace EachLabs
         /// <inheritdoc/>
         public global::EachLabs.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::EachLabs.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::EachLabs.AiModelsSourceGenerationContext.Default);
 
         /// <summary>
         ///
---
+++ src/libs/EachLabs/Generated/EachLabs.IWebhooksClient.Authorizations.ApiKeyInHeader.g.cs
@@ -0,0 +1,16 @@
+
+#nullable enable
+
+namespace EachLabs
+{
+    public partial interface IWebhooksClient
+    {
+        /// <summary>
+        /// Authorize using ApiKey authentication.
+        /// </summary>
+        /// <param name="apiKey"></param>
+
+        public void AuthorizeUsingApiKeyInHeader(
+            string apiKey);
+    }
+}
